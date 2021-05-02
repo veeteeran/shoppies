6696e3f8c532d0f0c789af99d7e9a7f1494a25d4
Viet <vietnamtran@gmail.com>
2021-05-02 18:47:47 -0500

    Add W3 modal

--- src/App.js
+++ src/App.js
@@ -74,6 +74,10 @@ function App() {
 
         const target = (nominateClick.target) ? nominateClick.target : nominateClick.srcElement;
         target.disabled = true;
+
+        if (nominationsList.childElementCount === 5) {
+            showModal();
+        }
     }
 
     const handleRemove = (event, nominateClick) => {
@@ -84,11 +88,19 @@ function App() {
         const nominateBtn = (nominateClick.target) ? nominateClick.target : nominateClick.srcElement;
         nominateBtn.disabled = false;
     }
+
+    const showModal = () => {
+        document.getElementById('id01').style.display='block'
+    }
+
+    const hideModal = () => {
+        document.getElementById('id01').style.display='none'
+    }
     
   return (
-    <div class='container'>
+    <div className='container'>
         <h1>The Shoppies</h1>
-        <div class='search-container'>
+        <div className='search-container'>
             <label htmlFor='title'>Movie title</label>
               <input
                   type='text'
@@ -99,20 +111,33 @@ function App() {
                   onKeyPress={handleKeypress}
               />
         </div>
-        <div class='bottom-box'>
-            <div class='results'>
+        <div className='bottom-box'>
+            <div className='results'>
                 <div id='results-text'>
                       Results
                 </div>    
                 <ul id='results-list'></ul>
             </div>
-            <div class='nominations'>
-                <div class='nominations-text'>
+            <div className='nominations'>
+                <div className='nominations-text'>
                       Nominations
                 </div>
                 <ul id='nominations-list'></ul>
             </div>
-      </div>
+          </div>
+        <div id="id01" className="w3-modal">
+            <div className="w3-modal-content">
+                <header className="w3-container w3-light-grey"> 
+                    <span onClick={hideModal}
+                    className="w3-button w3-display-topright">&times;</span>
+                    <h2>Your nominations</h2>
+                </header>
+                <div className="w3-container">
+                    <p>Thank you for your help with The Shoppies</p>
+                    {/* <p>Edit or submit your list</p> */}
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
